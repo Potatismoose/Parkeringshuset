dc51c3b2e86af1472f2aea42af2fe96024c96d4d
Jesper Persson <83016077+AndersJesperPersson@users.noreply.github.com>
2021-12-01 15:10:12 +0100

    Jesper parking ticket controller (#35)
    * Updated database structure. Once again..
    
    * fixed bugs
    
    * fixed bugs

--- Parkeringshuset/Helpers/SeedData.cs
+++ Parkeringshuset/Helpers/SeedData.cs
@@ -13,35 +13,8 @@
     {
 
         public static ParkeringGarageContext Db = new();
-        public static void Mock()
-        {
 
-        //    if (Db.pSpots.Any())
-        //    {
-               
-        //    }
-        //    else
-        //    {
-        //        // Initial seed to create how many parking spots that exsits in the garage. 
-        //        PSpot pSpot = new();
-        //        pSpot.Monthly = 30;
-        //        pSpot.Handicap = 10;
-        //        pSpot.Regular = 120;
-        //        pSpot.Motorbike = 10;
-        //        pSpot.Electric = 30;
-
-
-        //        Db.pSpots.Add(pSpot);
-        //        Db.SaveChanges();
-        //    }
-
-        //    var firstID = Db.pSpots.FirstOrDefault(p => p.Id == 1);
-        //    Console.WriteLine(firstID.Monthly); 
-
-
-        }
-
-        public static void TestPType()
+        public static void RunMock()
         {
             var hcp = Db.Ptypes.FirstOrDefault(p => p.Name == "Handicap");
             DisplayHelper.DisplayGreen(hcp.TotalSpots.ToString());
@@ -50,9 +23,9 @@
             {
                 Db.Ptypes.Add(new PType { Name = "Handicap", TotalSpots = 10 });
                 Db.Ptypes.Add(new PType { Name = "Regular", TotalSpots = 120 });
-                Db.Ptypes.Add(new PType { Name = "Electric", TotalSpots= 30 });
-                Db.Ptypes.Add(new PType { Name = "Monthly", TotalSpots= 30 });
-                Db.Ptypes.Add(new PType { Name = "Motorbike", TotalSpots=10 });
+                Db.Ptypes.Add(new PType { Name = "Electric", TotalSpots = 30 });
+                Db.Ptypes.Add(new PType { Name = "Monthly", TotalSpots = 30 });
+                Db.Ptypes.Add(new PType { Name = "Motorbike", TotalSpots = 10 });
 
                 Db.SaveChanges();
             }
@@ -61,8 +34,6 @@
                 Console.WriteLine(ex.Message);
             }
 
-
-           
         }
     }
 }
