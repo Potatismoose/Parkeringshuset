f167dcc1ece5980115a3764847bde4d117e325c0
Jesper Persson <83016077+AndersJesperPersson@users.noreply.github.com>
2021-12-02 19:55:53 +0100

    refactored calculate method

--- Parkeringshuset/BusinessLogic/ParkingMeterLogic.cs
+++ Parkeringshuset/BusinessLogic/ParkingMeterLogic.cs
@@ -75,164 +75,88 @@
 
         public double CalculateCost()
         {
-            //1.  00-06 = 5 kr    0.08
-            //2.  07-12 = 10 kr   0.16
-            //3.  13-17. = 20 kr   0.33   60 
-            //4.  18-24 = 10 kr   0.17
 
-            // checkintime  15.00    60 kr
-            // checkouttime  20.00   20 kr 
-            // 
-            var checkOut = new DateTime(2021, 12, 03, 09, 00, 00);   // checkout. 
-            DateTime incheck = new DateTime(2021, 12, 02, 19, 00, 00);  // checkint
+            DateTime checkOut = new DateTime(2021, 12, 03, 09, 00, 00);   // checkout. 
+            DateTime incheck = new DateTime(2021, 12, 02, 19, 00, 00);  // checkin
 
-            var totalTime = checkOut - incheck;
+            var totalTime = checkOut - incheck;             // get the parkingtime in hours and minutes.
+            var NumberOfDays = (int)totalTime.TotalDays;    // if the parking is less then 24 hours but over the night we need to add a day to make the for loop run correctly
 
-            var NumberOfDays = (int)totalTime.TotalDays;
-
-            if(NumberOfDays < 1 && incheck.Date != checkOut.Date)
+            if (NumberOfDays < 1 && incheck.Date != checkOut.Date)                                                                   
             {
                 NumberOfDays = 1;
             }
 
+            int morningMinutes = 0, lunchMinutes = 0 , evningMinutes = 0, nightMinutes = 0;   // declaring variabels I need down below.
+        
+            DateTime morningTime, lunchTime, evningTime, nightTime;
+            GetTimeSlotsInDateTime(out morningTime, out lunchTime, out evningTime, out nightTime);
 
-            var counterMorningMinutes = 0;
-            var counterLunchMinutes = 0;
-            var counterEvningMinutes = 0;
-            var counterNightMinutes = 0;
-            
-
-
-            DateTime morningTime = new DateTime(2021,01,01,06,00,00);
-            DateTime lunchTime = new DateTime(2021,01,01,12,00,00);
-            DateTime EvningTime = new DateTime(2021, 01, 01, 18, 00, 00);
-            DateTime NightTime = new DateTime(2021,01,01,23,59,00);
-
-
-            for (int i = 0; i <= NumberOfDays; i++)
+            for (int i = 0; i <= NumberOfDays; i++) 
             {
-                if(incheck.TimeOfDay < morningTime.TimeOfDay)  // 00-05.59
+                if (incheck.TimeOfDay < morningTime.TimeOfDay)  // 00-05.59
                 {
                     totalTime = checkOut - incheck;
-                    TimeSpan totalHoursInThisCategory = default;
-                    if (totalTime.TotalMinutes < 360)
-                    {
-                        totalHoursInThisCategory = totalTime;
-
-                    }
-                    else
-                    {
-                        totalHoursInThisCategory = morningTime.TimeOfDay - incheck.TimeOfDay;
-                    }
-                    counterMorningMinutes += (int)totalHoursInThisCategory.TotalMinutes;
-                    incheck += totalHoursInThisCategory;
+                    morningMinutes +=(int)MinutesInThisTimeSlot(incheck, totalTime, morningTime);
+                    incheck = incheck.AddMinutes(MinutesInThisTimeSlot(incheck, totalTime, morningTime));
                 }
 
                 if (incheck.TimeOfDay >= morningTime.TimeOfDay && incheck.TimeOfDay < lunchTime.TimeOfDay) // 06-12.59
                 {
                     totalTime = checkOut - incheck;
-                    TimeSpan totalHoursInThisCategory = default;
-                    if (totalTime.TotalMinutes < 360)
-                    {
-                        totalHoursInThisCategory = totalTime;
+                    lunchMinutes += (int)MinutesInThisTimeSlot(incheck, totalTime, lunchTime);
+                    incheck = incheck.AddMinutes(MinutesInThisTimeSlot(incheck, totalTime, lunchTime));
 
-                    }
-                    else
-                    {
-                        totalHoursInThisCategory = lunchTime.TimeOfDay - incheck.TimeOfDay;
-                    }
-
-                    counterLunchMinutes += (int)totalHoursInThisCategory.TotalMinutes;
-                    incheck += totalHoursInThisCategory;
-                    
                 }
 
-                if (incheck.TimeOfDay >= lunchTime.TimeOfDay && incheck.TimeOfDay < EvningTime.TimeOfDay) //13-17.59
+                if (incheck.TimeOfDay >= lunchTime.TimeOfDay && incheck.TimeOfDay < evningTime.TimeOfDay) //13-17.59
                 {
                     totalTime = checkOut - incheck;
-                    TimeSpan totalHoursInThisCategory = default;
-                    if (totalTime.TotalMinutes < 360)
-                    {
-                        totalHoursInThisCategory = totalTime;
-                        
-                    }
-                    else
-                    {
-                        totalHoursInThisCategory = EvningTime.TimeOfDay - incheck.TimeOfDay;
-                    }
-
-
-                    counterEvningMinutes += (int)totalHoursInThisCategory.TotalMinutes;
-                    incheck += totalHoursInThisCategory;
-                    
+                    evningMinutes += (int)MinutesInThisTimeSlot(incheck, totalTime, evningTime);
+                    incheck = incheck.AddMinutes(MinutesInThisTimeSlot(incheck, totalTime, evningTime));
+                 
                 }
 
-                if (incheck.TimeOfDay >= EvningTime.TimeOfDay)   //18-23.59
+                if (incheck.TimeOfDay >= evningTime.TimeOfDay)   //18-23.59
                 {
                     totalTime = checkOut - incheck;
-                    TimeSpan totalHoursInThisCategory = default;
-                    if (totalTime.TotalMinutes < 360)
-                    {
-                        totalHoursInThisCategory = totalTime;
-
-                    }
-                    else
-                    {
-                        totalHoursInThisCategory = NightTime.TimeOfDay - incheck.TimeOfDay;  
-                    }
-
-                    counterNightMinutes += (int)totalHoursInThisCategory.TotalMinutes;
-                    incheck += totalHoursInThisCategory;
-                    incheck = incheck.AddMinutes(1);
+                    nightMinutes += (int)MinutesInThisTimeSlot(incheck, totalTime, nightTime);
+                    incheck = incheck.AddMinutes(MinutesInThisTimeSlot(incheck, totalTime, nightTime) + 1);
+                     
                 }
-
-
             }
 
-           
-            var test = (counterNightMinutes * 0.166666666); 
-
-            var sum = (counterMorningMinutes * 0.0833333333) + 
-                (counterLunchMinutes * 0.166666666) +
-                (counterEvningMinutes * 0.3333333333) + 
-                (counterNightMinutes * 0.166666666);
-
-            return Math.Round(sum);
+            return Math.Round(
+                 (morningMinutes * 0.0833333333) +
+                (lunchMinutes * 0.166666666) +
+                (evningMinutes * 0.3333333333) +
+                (nightMinutes * 0.166666666));
 
 
+        }
 
-            //double sum = 0;
-            //var resut = temp2 - temp;  // 4 h. 
+        private static double MinutesInThisTimeSlot(DateTime incheck, TimeSpan totalTime, DateTime CategoryTime)
+        {
+            TimeSpan totalHoursInThisCategory;
+            if (totalTime.TotalMinutes <= 360)
+            {
+                totalHoursInThisCategory = totalTime;
 
-            //if(temp.TimeOfDay < morningTime.TimeOfDay)
-            //{
-            //    Console.WriteLine("Morgontid 00-06, 5 kr.");
+            }
+            else
+            {
+                totalHoursInThisCategory = CategoryTime.TimeOfDay - incheck.TimeOfDay;
+            }
 
-            //}
-            //if (temp.TimeOfDay > morningTime.TimeOfDay && temp.TimeOfDay < lunchTime.TimeOfDay)
-            //{
-            //    Console.WriteLine("Lunchtid 10 kr. 07-12");
-            //}
-            //if (temp.TimeOfDay > lunchTime.TimeOfDay && temp.TimeOfDay < EvningTime.TimeOfDay)
-            //{
-            //    Console.WriteLine("Evning 20 kr, 13-18");
-            //    var result = EvningTime.TimeOfDay - temp.TimeOfDay;
-            //    int minutesInINt = (int)result.TotalMinutes;
-            //    double CostPerMinute = 0.34;
-            //    sum += minutesInINt * CostPerMinute;
-            //    temp += result;
+            return totalHoursInThisCategory.TotalMinutes;
+        }
 
-            //}
-            //if (temp.TimeOfDay > EvningTime.TimeOfDay)
-            //{
-            //    Console.WriteLine("Natt 10 kr, 18-24");
-            //    var result = NightTime.TimeOfDay - temp.TimeOfDay;
-            //    int minutesInINt = (int)result.TotalMinutes;
-            //    double CostPerMinute = 0.34;
-            //    sum += minutesInINt * CostPerMinute;
-            //    temp -= result;
-            //}
-
+        private static void GetTimeSlotsInDateTime(out DateTime morningTime, out DateTime lunchTime, out DateTime EvningTime, out DateTime NightTime)
+        {
+            morningTime = new DateTime(2021, 01, 01, 06, 00, 00);
+            lunchTime = new DateTime(2021, 01, 01, 12, 00, 00);
+            EvningTime = new DateTime(2021, 01, 01, 18, 00, 00);
+            NightTime = new DateTime(2021, 01, 01, 23, 59, 00);
         }
 
         private static bool IsCardCredentialsValid(string cardInfo, string CSV)
