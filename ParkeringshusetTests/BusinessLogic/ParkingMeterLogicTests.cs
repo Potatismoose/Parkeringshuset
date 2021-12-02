73b804d97865df2ec0e31627c8088ef40fd343ef
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-12-02 15:23:17 +0100

    Added NUnit test class for ParkingMeterLogic.cs

---
+++ ParkeringshusetTests/BusinessLogic/ParkingMeterLogicTests.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkeringshusetTests.BusinessLogic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class ParkingMeterLogicTests
+    {
+    }
+}
