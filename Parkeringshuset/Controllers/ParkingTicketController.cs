b453ec2b7ede731a095c966ebcc1c36543386a5d
jerry1938 <49976147+jerry1938@users.noreply.github.com>
2021-12-03 13:51:09 +0100

    Created IsTicketActive, IsPayed and IsMontly methods.

--- Parkeringshuset/Controllers/ParkingTicketController.cs
+++ Parkeringshuset/Controllers/ParkingTicketController.cs
@@ -86,75 +86,52 @@ namespace Parkeringshuset.Models
 
             return ticket;
         }
+
+        /// <summary>
+        /// Checks if the ticket is active.
+        /// </summary>
+        /// <param name="ticket">The ticket to check.</param>
+        /// <returns>True if the ticket is active, otherwise false.</returns>
+        public bool IsTicketActive(PTicket ticket){
+            var t = db.Ptickets.FirstOrDefault(x => x.CheckedOutTime == DateTime.MinValue);
+            return ticket.CheckedOutTime == t.CheckedOutTime;
+        }
+
+        // public bool IsPayed(string regNr){
+        //     var vehicle = db.Vehicles.FirstOrDefault(x => x.RegistrationNumber == regNr);
+
+        //     if (vehicle == null)
+        //     {
+        //         return false;
+        //     }
+
+        //     var ticket = db.Ptickets.FirstOrDefault(x => x.Vehicle.Id == vehicle.Id);
+        //     if (IsTicketActive(ticket)){
+        //         if (!ticket.IsPaid)
+        //         {
+        //             return false;
+        //         }
+        //         return false;
+        //     }
+        //     return true;
+        // }
+
+        /// <summary>
+        /// Check if the ticket is monthly.
+        /// </summary>
+        /// <param name="regNr">The reqNr of the vehicle.</param>
+        /// <returns>True if the ticket is monthly otherwise false.</returns>
+        public bool IsMonthly(string regNr){
+            var vehicle = db.Vehicles.FirstOrDefault(x => x.RegistrationNumber == regNr);
+
+            if (vehicle == null)
+            {
+                return false;
+            }
+
+            var ticket = db.Ptickets.FirstOrDefault(x => x.Vehicle.Id == vehicle.Id);
+
+            return ticket.Type.Name == "Monthly";
+        }
     }
 }
-//private Vehicle AssociatedVehicle;
-//private DateTime ArrivalTime;
-//private DateTime? CheckoutTime = null;
-//private int PricePerStartedHour;
-
-//public ParkingTicketController(Vehicle associatedVehicle, int hourlyCost)
-//{
-//    ArrivalTime = DateTime.Now;
-//    AssociatedVehicle = associatedVehicle;
-//    PricePerStartedHour = hourlyCost;
-//}
-
-//public ParkingTicketController Checkout()
-//{
-//    CheckoutTime = DateTime.Now;
-//    CalculateCost();
-//    return this;
-//}
-
-//public int CalculateCost()
-//{
-//    TimeSpan time = new(1, 0, 0);
-//    time += Convert.ToDateTime(CheckoutTime) - ArrivalTime;
-//    return time.Hours * PricePerStartedHour;
-//}
-
-//public bool TicketIsStillActive()
-//{
-//    return !CheckoutTime.HasValue;
-//}
-
-//public bool IsSameVehicle(Vehicle vehicle)
-//{
-//    return string.Equals(
-//        AssociatedVehicle.ToString(),
-//        vehicle.ToString(),
-//        StringComparison.OrdinalIgnoreCase);
-//}
-
-//public bool IsPayed()
-//{
-//    return CheckoutTime.HasValue;
-//}
-
-//public DateTime ReturnArrivalTime()
-//{
-//    return ArrivalTime;
-//}
-
-//public DateTime? ReturnCheckoutTime()
-//{
-//    return CheckoutTime;
-//}
-
-//public override string ToString()
-//{
-//    var cost = 0;
-//    if (CheckoutTime.HasValue)
-//    {
-//        return $"Biljett. " +
-//            $"Regnr: {AssociatedVehicle}, " +
-//            $"{(cost = CalculateCost()) / PricePerStartedHour}tim " +
-//            $"á {PricePerStartedHour}kr. " +
-//            $"\tTot {cost}kr";
-//    }
-//    else 
-//    {
-//        return $"Regnr: {AssociatedVehicle}, Hourly cost: {PricePerStartedHour}kr. Parking started: {ArrivalTime} ";
-//    }
-//}
