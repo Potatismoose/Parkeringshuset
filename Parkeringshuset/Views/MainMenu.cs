738e22ab0a442b07a6515883b1df7310ac7fa3a6
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-12-06 13:55:37 +0100

    Added code to RunMainMenu.

--- Parkeringshuset/Views/MainMenu.cs
+++ Parkeringshuset/Views/MainMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System;
+using Parkeringshuset.Models;
 
 namespace Parkeringshuset.Views
 {
@@ -14,9 +15,31 @@ namespace Parkeringshuset.Views
             {
                 Console.WriteLine("Can not use empty registration number, please try again.");
             }
-            if (ParkingMeterLogic.CheckIn(regNr))
+            if (ParkingMeterController.GetActiveTicket(regNr) == null)
             {
-
+                ParkingTicketController.CheckIn(regNr);
+                Console.WriteLine($"Checked In, what zone would you like to park in?: ");
+                Console.WriteLine("1. Normal vehicle");
+                Console.WriteLine("2. Electric vehicle");
+                Console.WriteLine("3. Handicaped");
+                Console.WriteLine("4. Monthly");
+                Console.WriteLine("5. Motorcycle");
+                int.TryParse(Console.ReadLine(), out int choice);
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine();
+                    default:
+                        break;
+                }
+            }
+            else if (ParkingMeterController.GetActiveTicket(regNr).Type.Name == "Monthly")
+            {
+                Console.WriteLine("Welcome back!");
+            }
+            else
+            {
+                ParkingTicketController.Checkout(regNr);
             }
 
 
