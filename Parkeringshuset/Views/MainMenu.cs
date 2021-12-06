5630c7e888e600e1e2a797375a443c1f7657c363
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-12-06 15:03:06 +0100

    Reformated MainMenu.

--- Parkeringshuset/Views/MainMenu.cs
+++ Parkeringshuset/Views/MainMenu.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
 using System;
 using Parkeringshuset.Models;
+using Parkeringshuset.BusinessLogic;
 
 namespace Parkeringshuset.Views
 {
     public static class MainMenu
     {
+        /// <summary>
+        /// Check in and out menu for users of the parking garage.
+        /// </summary>
         public static void RunMainMenu()
         {
             bool keepGoing = true;
+            string pType = "";
+            ParkingMeterLogic pML = new ();
             do
             {
                 Console.Write("Enter your registration number: ");
@@ -21,34 +27,46 @@ namespace Parkeringshuset.Views
                 var parkingTicket = ParkingMeterController.GetActiveTicket(regNr);
                 if (parkingTicket is null)
                 {
-                    ParkingTicketController.CheckIn(regNr);
                     Console.WriteLine($"Checked In, what zone would you like to park in?: ");
-                    Console.WriteLine("1. Normal vehicle");
+                    Console.WriteLine("1. Regular vehicle");
                     Console.WriteLine("2. Electric vehicle");
                     Console.WriteLine("3. Handicaped");
                     Console.WriteLine("4. Monthly");
-                    Console.WriteLine("5. Motorcycle");
+                    Console.WriteLine("5. Motorbike");
                     Console.WriteLine("6. Abort check in");
                     int.TryParse(Console.ReadLine(), out int choice);
                     switch (choice)
                     {
                         case 1:
-                            Console.WriteLine();
+                            pType = "Regular";
+                            pML.CheckIn(regNr, pType);
+                            PressAnyKeyToContinue();
                             break;
                         case 2:
-                            Console.WriteLine();
+                            pType = "Electric";
+                            pML.CheckIn(regNr, pType);
+                            PressAnyKeyToContinue();
                             break;
                         case 3:
-                            Console.WriteLine();
+                            pType = "Handicap";
+                            pML.CheckIn(regNr, pType);
+                            PressAnyKeyToContinue();
                             break;
                         case 4:
-                            Console.WriteLine();
+                            pType = "Monthly";
+                            pML.CheckIn(regNr, pType);
+                            PressAnyKeyToContinue();
                             break;
                         case 5:
-                            Console.WriteLine();
+                            pType = "Motorbike";
+                            pML.CheckIn(regNr, pType);
+                            PressAnyKeyToContinue();
                             break;
-
+                        case 6:
+                            keepGoing = false;
+                            break;
                         default:
+                            Console.WriteLine("Wrong input please try again.");
                             break;
                     }
                 }
@@ -56,16 +74,22 @@ namespace Parkeringshuset.Views
                 {
                     Console.WriteLine("Welcome back!");
                     keepGoing = false;
+                    PressAnyKeyToContinue();
                 }
                 else
                 {
                     ParkingTicketController.CheckOut(parkingTicket);
                     Console.WriteLine("Thank you for using our garage, welcome back");
                     keepGoing = false;
+                    PressAnyKeyToContinue();
                 }
             } while (keepGoing);
+        }
 
-
+        private static void PressAnyKeyToContinue()
+        {
+            Console.WriteLine("Press any key to continue. . .");
+            Console.ReadKey();
         }
     }
 }
