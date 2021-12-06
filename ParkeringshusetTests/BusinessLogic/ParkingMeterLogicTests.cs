33bd08c0287bda69b624cedd0a168f00fc822538
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-12-06 14:37:34 +0100

    Beginning of implementation of more tests

--- ParkeringshusetTests/BusinessLogic/ParkingMeterLogicTests.cs
+++ ParkeringshusetTests/BusinessLogic/ParkingMeterLogicTests.cs
@@ -7,8 +7,25 @@ namespace Parkeringshuset.BusinessLogic.Tests
     [TestFixture()]
     public class ParkingMeterLogicTests
     {
+        //private static Setup()
+        //{
+        //    DateTime dT1 = new DateTime(2021, 12, 01, 12, 00, 00);
+        //}
+
+        [TestCase("2021-12-01T12:00:00", "2021-12-02T12:00:00", 270)]
         [Test()]
-        public void CalculateCost()
+        public void CalculateCostTest_ShouldBeEqual_WhenGivenCorrectValues(
+            DateTime checkIn,
+            DateTime checkOut,
+            int expected)
+        {
+            var result = CalculateCostLogic.Cost(checkIn, checkOut);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void CalculateCostTest_()
         {
             DateTime checkIn = new DateTime(2021, 12, 01, 12, 00, 00);
             DateTime checkOut = new DateTime(2021, 12, 02, 12, 00, 00);
