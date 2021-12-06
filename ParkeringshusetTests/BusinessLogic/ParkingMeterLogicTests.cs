2037c6cdd8a5d83e37426e989b315d98cf72d62e
Jesper Persson <83016077+AndersJesperPersson@users.noreply.github.com>
2021-12-06 19:38:25 +0100

    Viktor jesper parking meter logic. (#47)
    * Beginning of implementation of more tests
    
    * Implemented IsMonthly and IsCarParked in Businesslogic.
    
    * trouble shooting. Not Done
    
    * converting datetime.
    
    * updated test.
    
    * updated test.
    
    * updated test.
    
    Co-authored-by: Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>

--- ParkeringshusetTests/BusinessLogic/ParkingMeterLogicTests.cs
+++ ParkeringshusetTests/BusinessLogic/ParkingMeterLogicTests.cs
@@ -1,20 +1,35 @@
 ﻿using NUnit.Framework;
 using Parkeringshuset.BusinessLogic;
 using System;
+using System.Globalization;
 
 namespace Parkeringshuset.BusinessLogic.Tests
 {
     [TestFixture()]
     public class ParkingMeterLogicTests
     {
+
+
+        [TestCase("2021/05/28 07:00", "2021/05/28 08:00", 10)]
+        //[TestCase("2021-12-01T12:00:00", "2021-12-02T1:00:00", 270)]
+        //[TestCase("2021-12-01T12:00:00", "2021-12-02T15:00:00", 270)]
         [Test()]
-        public void CalculateCost()
+        public void CalculateCostTest_ShouldBeEqual_WhenGivenCorrectValues(
+            DateTime checkIn,
+            DateTime checkOut,
+            int expected)
         {
-            DateTime checkIn = new DateTime(2021, 12, 01, 12, 00, 00);
-            DateTime checkOut = new DateTime(2021, 12, 02, 12, 00, 00);
+
+            // Acording to all documentation, this should work but dosen´t.. haha
+           // string format = "yyyy-MM-dd HH:mm:ss";
+           //CultureInfo cultureinfo =
+           //      CultureInfo.CreateSpecificCulture("en-US");
+           // var test = DateTime.ParseExact(checkIn.ToString(),format, cultureinfo);
+
             var result = CalculateCostLogic.Cost(checkIn, checkOut);
 
-            Assert.AreEqual(270, result);
+            Assert.AreEqual(expected, result);
         }
+
     }
 }
