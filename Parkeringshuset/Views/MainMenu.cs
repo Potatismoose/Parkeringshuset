f2be4c4bc2e435a09d459be88b445d67a039d007
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-12-06 14:20:24 +0100

    Reformated RunMainMenu.

--- Parkeringshuset/Views/MainMenu.cs
+++ Parkeringshuset/Views/MainMenu.cs
@@ -6,41 +6,64 @@ namespace Parkeringshuset.Views
 {
     public static class MainMenu
     {
-
         public static void RunMainMenu()
         {
-            Console.WriteLine("Enter your registration number: ");
-            string regNr = Console.ReadLine();
-            if (string.IsNullOrEmpty(regNr.Trim()))
+            bool keepGoing = true;
+            do
             {
-                Console.WriteLine("Can not use empty registration number, please try again.");
-            }
-            if (ParkingMeterController.GetActiveTicket(regNr) == null)
-            {
-                ParkingTicketController.CheckIn(regNr);
-                Console.WriteLine($"Checked In, what zone would you like to park in?: ");
-                Console.WriteLine("1. Normal vehicle");
-                Console.WriteLine("2. Electric vehicle");
-                Console.WriteLine("3. Handicaped");
-                Console.WriteLine("4. Monthly");
-                Console.WriteLine("5. Motorcycle");
-                int.TryParse(Console.ReadLine(), out int choice);
-                switch (choice)
+                Console.Write("Enter your registration number: ");
+                string regNr = Console.ReadLine();
+                if (string.IsNullOrEmpty(regNr.Trim()))
                 {
-                    case 1:
-                        Console.WriteLine();
-                    default:
-                        break;
+                    Console.WriteLine("Can not use empty registration number, please try again.");
+                    continue;
                 }
-            }
-            else if (ParkingMeterController.GetActiveTicket(regNr).Type.Name == "Monthly")
-            {
-                Console.WriteLine("Welcome back!");
-            }
-            else
-            {
-                ParkingTicketController.Checkout(regNr);
-            }
+                var parkingTicket = ParkingMeterController.GetActiveTicket(regNr);
+                if (parkingTicket is null)
+                {
+                    ParkingTicketController.CheckIn(regNr);
+                    Console.WriteLine($"Checked In, what zone would you like to park in?: ");
+                    Console.WriteLine("1. Normal vehicle");
+                    Console.WriteLine("2. Electric vehicle");
+                    Console.WriteLine("3. Handicaped");
+                    Console.WriteLine("4. Monthly");
+                    Console.WriteLine("5. Motorcycle");
+                    Console.WriteLine("6. Abort check in");
+                    int.TryParse(Console.ReadLine(), out int choice);
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine();
+                            break;
+                        case 2:
+                            Console.WriteLine();
+                            break;
+                        case 3:
+                            Console.WriteLine();
+                            break;
+                        case 4:
+                            Console.WriteLine();
+                            break;
+                        case 5:
+                            Console.WriteLine();
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                else if (parkingTicket.Type.Name == "Monthly")
+                {
+                    Console.WriteLine("Welcome back!");
+                    keepGoing = false;
+                }
+                else
+                {
+                    ParkingTicketController.CheckOut(parkingTicket);
+                    Console.WriteLine("Thank you for using our garage, welcome back");
+                    keepGoing = false;
+                }
+            } while (keepGoing);
 
 
         }
