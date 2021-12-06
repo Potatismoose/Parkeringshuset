45fa447ca4f2f989e492a6f43c3a54c6420b7e53
Jesper Persson <83016077+AndersJesperPersson@users.noreply.github.com>
2021-12-06 19:31:21 +0100

    updated test.

--- ParkeringshusetTests/BusinessLogic/ParkingMeterLogicTests.cs
+++ ParkeringshusetTests/BusinessLogic/ParkingMeterLogicTests.cs
@@ -21,10 +21,10 @@ namespace Parkeringshuset.BusinessLogic.Tests
         {
 
             // Acording to all documentation, this should work but dosen´t.. haha
-            string format = "yyyy-MM-dd HH:mm:ss";
-           CultureInfo cultureinfo =
-                 CultureInfo.CreateSpecificCulture("en-US");
-            var test = DateTime.ParseExact(checkIn.ToString(),format, cultureinfo);
+           // string format = "yyyy-MM-dd HH:mm:ss";
+           //CultureInfo cultureinfo =
+           //      CultureInfo.CreateSpecificCulture("en-US");
+           // var test = DateTime.ParseExact(checkIn.ToString(),format, cultureinfo);
 
             var result = CalculateCostLogic.Cost(test, checkOut);
 
