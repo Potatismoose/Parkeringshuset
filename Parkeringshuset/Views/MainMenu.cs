83df3fba1c67fcc9a6be2f09440d0b86966b7eca
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-12-13 19:43:55 +0100

    Bug might be taken care of

--- Parkeringshuset/Views/MainMenu.cs
+++ Parkeringshuset/Views/MainMenu.cs
@@ -5,6 +5,7 @@ using Parkeringshuset.BusinessLogic;
 using System.Linq;
 using static System.ConsoleKey;
 using System.Text.RegularExpressions;
+using Parkeringshuset.Helpers;
 
 namespace Parkeringshuset.Views
 {
@@ -29,79 +30,87 @@ namespace Parkeringshuset.Views
             bool keepGoing = true;
             bool isValidRegNr = false;
             bool isCharOrDigit = false;
-            bool isLoginSuccessful = false;
-            bool haveAdminLoggedOut = false;
+            bool hasAdminLoggedOut = false;
             string pType = "";
             string regNr = "";
+            string confirmedRegNr = "";
 
             ParkingMeterLogic pML = new();
             ParkingTicketController pTC = new();
             do
             {
+                isValidRegNr = false;
+                hasAdminLoggedOut = false;
                 Console.Clear();
                 Console.Write("Enter your registration number: ");
                 do
                 {
-                    while (Console.KeyAvailable) { Console.ReadKey(false); }
                     ConsoleKeyInfo pressedKey = Console.ReadKey();
                     secretPatternMatch.Add(pressedKey.Key);
 
-                    foreach (var item in secretPatternMatch)
+                    if (pressedKey.Key != RightArrow &&
+                        pressedKey.Key != LeftArrow &&
+                        pressedKey.Key != UpArrow &&
+                        pressedKey.Key != DownArrow
+                    )
                     {
-                        if (item != RightArrow &&
-                            item != LeftArrow &&
-                            item != UpArrow &&
-                            item != DownArrow
-                        )
+                        isCharOrDigit = true;
+                        regNr += (char)pressedKey.Key;
+                        if (regNr.Length >= 2 && regNr.Length <= 7 && pressedKey.Key == Enter)
                         {
-                            isCharOrDigit = true;
-                            regNr += (char)pressedKey.Key;
-                            if (regNr.Length >= 2 && regNr.Length <= 7 && pressedKey.Key == Enter)
-                            {
-                                isValidRegNr = true;
-                            }
-                        }
-                        else
-                        {
-                            isCharOrDigit = false;
+                            isValidRegNr = true;
+                            confirmedRegNr = regNr;
+                            regNr = "";
+                            break;
                         }
                     }
+                    else
+                    {
+                        isCharOrDigit = false;
+                    }
+
                     if (isCharOrDigit)
                     {
                         secretPatternMatch.Clear();
                     }
-                    else if (secretPatternMatch.SequenceEqual(secretPattern))
+                    if (secretPatternMatch.SequenceEqual(secretPattern))
                     {
+                        secretPatternMatch.Clear();
                         AdminMenu login = new();
-                        if (isLoginSuccessful == login.LoginAdmin())
+                        if (login.LoginAdmin())
                         {
                             login.PrintAdminPage();
-                            haveAdminLoggedOut = true;
+                            hasAdminLoggedOut = true;
                         }
                         else
                         {
+                            hasAdminLoggedOut = true;
                             Helper.DisplayHelper.DisplayRed("Something went wrong!");
+                            PressAnyKeyToContinue();
                         }
-                        secretPatternMatch.Clear();
                     }
-                } while (!isValidRegNr && !secretPatternMatch.SequenceEqual(secretPattern));
+                    if (hasAdminLoggedOut)
+                    {
+                        break;
+                    }
+                } while (!isValidRegNr || !secretPatternMatch.SequenceEqual(secretPattern));
 
-                if (haveAdminLoggedOut)
+                if (hasAdminLoggedOut)
                 {
-                    haveAdminLoggedOut = false;
                     continue;
                 }
 
-                if (string.IsNullOrEmpty(regNr.Trim()))
+                if (string.IsNullOrEmpty(confirmedRegNr.Trim()))
                 {
                     Console.WriteLine("Can not use empty registration number, please try again.");
                     PressAnyKeyToContinue();
                     continue;
                 }
-                var parkingTicket = pTC.GetActiveTicket(regNr);
+                var parkingTicket = pTC.GetActiveTicket(confirmedRegNr);
 
                 if (parkingTicket is null)
                 {
+                    Console.WriteLine(confirmedRegNr);
                     Console.WriteLine($"Checked In, what zone would you like to park in?: ");
                     Console.WriteLine("1. Regular vehicle");
                     Console.WriteLine("2. Electric vehicle");
@@ -114,19 +123,19 @@ namespace Parkeringshuset.Views
                     {
                         case 1:
                             pType = "Regular";
-                            pML.CheckIn(regNr, pType);
+                            pML.CheckIn(confirmedRegNr, pType);
                             PressAnyKeyToContinue();
                             break;
 
                         case 2:
                             pType = "Electric";
-                            pML.CheckIn(regNr, pType);
+                            pML.CheckIn(confirmedRegNr, pType);
                             PressAnyKeyToContinue();
                             break;
 
                         case 3:
                             pType = "Handicap";
-                            pML.CheckIn(regNr, pType);
+                            pML.CheckIn(confirmedRegNr, pType);
                             PressAnyKeyToContinue();
                             break;
                         //case 4:
@@ -136,7 +145,7 @@ namespace Parkeringshuset.Views
                         //    break;
                         case 5:
                             pType = "Motorbike";
-                            pML.CheckIn(regNr, pType);
+                            pML.CheckIn(confirmedRegNr, pType);
                             PressAnyKeyToContinue();
                             break;
 
@@ -150,12 +159,10 @@ namespace Parkeringshuset.Views
                             break;
                     }
                     secretPatternMatch.Clear();
-                    regNr = "";
                 }
                 else if (pTC.IsMonthly(parkingTicket) && pTC.IsTicketActive(parkingTicket))
                 {
                     Console.WriteLine($"Welcome back! Your ticket expires { parkingTicket.CheckedOutTime}");
-                    regNr = "";
                     secretPatternMatch.Clear();
                     PressAnyKeyToContinue();
                 }
@@ -163,7 +170,6 @@ namespace Parkeringshuset.Views
                 {
                     pTC.CheckOut(parkingTicket);
                     Console.WriteLine("Checked out. Thank you for using our garage, welcome back!");
-                    regNr = "";
                     secretPatternMatch.Clear();
                     PressAnyKeyToContinue();
                 }
