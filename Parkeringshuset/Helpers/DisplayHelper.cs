d3108ff84ec8546e9d3c3d382d0c2b7df863d2e7
Rauf84 <rauf@karimli.se>
2021-12-14 20:17:06 +0100

    Refactoring TicketHelpers & DisplayHelper.cs.

--- Parkeringshuset/Helpers/DisplayHelper.cs
+++ Parkeringshuset/Helpers/DisplayHelper.cs
@@ -1,27 +1,24 @@
 ﻿namespace Parkeringshuset.Helper
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public static class DisplayHelper
     {
-
+        /// <summary>
+        /// Changes console color to red in case of wrong input.
+        /// </summary>
+        /// <param name="text"></param>
         public static void DisplayRed(string text)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(text);
             Console.ResetColor();
-        
         }
         public static void DisplayGreen(string text)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(text);
             Console.ResetColor();
-
         }
     }
 }
