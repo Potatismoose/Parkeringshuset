dfb3c591781340587ff3cb4dd99460346e96454f
Rauf84 <74669492+Rauf84@users.noreply.github.com>
2021-12-14 19:08:36 +0100

    Rauf refactoring (#84)
    * Refactored MainMenu.cs + added XML comment.
    
    * Refactoring Admin.cs
    
    * Refactoring PSpot
    
    * Refactoring PTicket.cs.
    
    * Refactoring PType.cs.
    
    * Refactoring Vehicle.cs.

--- Parkeringshuset/Models/Admin.cs
+++ Parkeringshuset/Models/Admin.cs
@@ -14,13 +14,12 @@
         [Key]
         public int Id { get; set; }
 
-       
         public string Username { get; set; }
-
         
         public string Password { get; set; }
 
         public string Email { get; set; }
+
         public string Salt { get; set; }
     }
 }
