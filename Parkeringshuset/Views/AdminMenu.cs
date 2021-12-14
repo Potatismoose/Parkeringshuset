d7346e1438166a7098df26e6e18c376660ee2bb2
Benny Christensen <63840665+Potatismoose@users.noreply.github.com>
2021-12-14 08:01:03 -0500

    Benny hocke test branch (#81)
    * Created 2 tests
    
    * Unit test for IsMonthly created.
    
    * Integrationtest created
    
    * Update db
    
    * Fixed bug
    
    * Update dotnet.yml
    
    * Testing bugfix
    
    * Reformated.
    
    * Fixed test...
    
    * Fixed names..
    
    * Update dotnet.yml
    
    * Hjärtinfarkt.
    
    * TestUpdate.
    
    * 919 Röster på stack, måste fungera.
    
    * Update test.
    
    * Testing to make HtmlCreator an object instead of static
    
    * Updated pdf and HTML tickethelper classes
    
    * Fixed bugs
    
    * Bugfix
    
    * Updated
    
    * Uncommented teardown that previously did not work
    
    * Fixed adminMenu
    
    Co-authored-by: Devhocke <71835476+DevHocke@users.noreply.github.com>

--- Parkeringshuset/Views/AdminMenu.cs
+++ Parkeringshuset/Views/AdminMenu.cs
@@ -27,18 +27,26 @@ namespace Parkeringshuset.Views
                 Console.WriteLine("6. Get comprehensive report");
                 Console.WriteLine("7. Logout");
                 int.TryParse(Console.ReadLine(), out int choice);
+                DateTime startDate;
+                DateTime endDate;
                 switch (choice)
                 {
                     case 1:
-                        afl.ParkingSportsPopularity();
+                        //afl.ParkingSportsPopularity(admin);
                         break;
 
                     case 2:
-                        afl.SoldTicketsBetweenSpecificDates();
+                        //afl.SoldTicketsBetweenSpecificDates(admin, startDate, endDate);
                         break;
 
                     case 3:
-                        //afl.Revenue();
+                        
+                        startDate = AskForStartOrEndDate("Start");
+                        endDate = AskForStartOrEndDate("End");
+                        Console.WriteLine(startDate);
+                        Console.WriteLine(endDate);
+                        Console.ReadKey();
+                        //afl.Revenue(admin, startDate, endDate);
                         break;
 
                     case 4:
@@ -46,7 +54,7 @@ namespace Parkeringshuset.Views
                         break;
 
                     case 5:
-                        afl.BestCustomer();
+                        //afl.BestCustomer(admin);
                         break;
 
                     case 6:
@@ -58,6 +66,37 @@ namespace Parkeringshuset.Views
                         break;
                 }
             }
+        }
+
+        private static DateTime AskForStartOrEndDate(string startOrEnd)
+        {
+            int Year;
+            int Month;
+            int Day;
+            bool isNumber;
+            do
+            {
+                Console.Write($"{startOrEnd}date - Year (YYYY): ");
+                isNumber = int.TryParse(Console.ReadLine(), out int year);
+                Year = year;
+            } while (!isNumber);
+
+            do
+            {
+                Console.Write($"{startOrEnd}date - Month (MM): ");
+                isNumber = int.TryParse(Console.ReadLine(), out int month);
+                Month = month;
+            } while (!isNumber);
+
+            do
+            {
+                Console.Write($"{startOrEnd}date - Day (DD): ");
+                isNumber = int.TryParse(Console.ReadLine(), out int day);
+                Day = day;
+            } while (!isNumber);
+
+
+            return new DateTime(Year, Month, Day);
         }
 
         /// <summary>
