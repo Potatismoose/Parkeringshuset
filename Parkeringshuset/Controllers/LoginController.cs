8bd1a0fbc5c15c11a0fca29c99596d828cd617cd
Jesper Persson <83016077+AndersJesperPersson@users.noreply.github.com>
2021-12-14 13:41:47 +0100

    Jerry login controller (#80)
    * Created the IsLoginSuccessful method.
    
    * Created unit tests for the IsLoginSuccessful method and added a user in the database.
    
    * Created a salt and hasing method.
    
    * Created Hasing add Salting.
    
    * updated database
    
    * Updated the database
    
    * Updated database
    
    Co-authored-by: jerry1938 <49976147+jerry1938@users.noreply.github.com>

--- Parkeringshuset/Controllers/LoginController.cs
+++ Parkeringshuset/Controllers/LoginController.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using Parkeringshuset.Data;
 using Parkeringshuset.Models;
 
@@ -18,6 +22,31 @@ namespace Parkeringshuset.Controllers
             var admin = db.Admins.FirstOrDefault(x => x.Username == username && x.Password == password);
 
             return admin;
+        }
+
+        /// <summary>
+        /// Generates a random number of nonzero values.
+        /// </summary>
+        /// <param name="size">The size of the salt.</param>
+        /// <returns>A byte[] with the salt.</returns>
+        public byte[] GenerateSalt(int size){
+            byte[] salt = new byte[size];
+            var rng = RandomNumberGenerator.Create();
+            rng.GetNonZeroBytes(salt);
+
+            return salt;
+        }
+
+        public string GenerateSha256(string password, byte[] salt){
+            byte[] pass = Encoding.ASCII.GetBytes(password);
+            List<byte> tmp = new();
+            tmp.AddRange(pass);
+            tmp.AddRange(salt);
+            byte[] hashedPassword = new byte[tmp.Count];
+
+            var sha256 = SHA256.Create();
+            sha256.ComputeHash(hashedPassword);
+            return Convert.ToBase64String(sha256.Hash) + Convert.ToBase64String(salt);
         }
     }
 }
