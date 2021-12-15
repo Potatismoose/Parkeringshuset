191d077c3bb3b8d4502a96244ab77480899c6df9
Rauf84 <74669492+Rauf84@users.noreply.github.com>
2021-12-15 19:26:55 +0100

    Rauf refactoring 3 (#101)
    * Refactored Models - done.
    
    * Refactoring Migrations & Program.cs
    
    * Refactoring EmailHelper,SendEmail.cs.
    
    * Refactoring TicketHelpers & DisplayHelper.cs.
    
    * Add XML comments in DisplayHelper.cs.
    
    * Refactoring ParkingTypesNames.cs.
    
    * Refactoring SeedData.cs and add XML comments.
    
    * Add XML comment to AdminMenu.cs.
    
    * Accept merge

--- Parkeringshuset/Views/AdminMenu.cs
+++ Parkeringshuset/Views/AdminMenu.cs
@@ -68,6 +68,11 @@ namespace Parkeringshuset.Views
             }
         }
 
+        /// <summary>
+        /// Return correct date for a ticket.
+        /// </summary>
+        /// <param name="startOrEnd"></param>
+        /// <returns></returns>
         private static DateTime AskForStartOrEndDate(string startOrEnd)
         {
             int Year;
@@ -99,7 +104,7 @@ namespace Parkeringshuset.Views
         }
 
         /// <summary>
-        /// Asks for admin credentials
+        /// Asks for admin credentials.
         /// </summary>
         /// <returns>True if admin is found</returns>
         public Admin LoginAdmin()
