67fa420877acf88477768ae4a9c55b2da4ba0c54
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-12-21 20:51:45 +0100

    Integration tests coming along

---
+++ ParkeringshusetTests/IntegrationTests/AdminLoginAndGetReportIntegrationTests.cs
@@ -0,0 +1,46 @@
+﻿namespace ParkeringshusetTests.IntegrationTests
+{
+    using NUnit.Framework;
+    using Parkeringshuset.BusinessLogic;
+    using Parkeringshuset.Controllers;
+    using Parkeringshuset.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [Category("IntegrationTests")]
+    [TestFixture()]
+    public class AdminLoginAndGetReportIntegrationTests
+    {
+        private LoginController _lC;
+        private AdminFunctionsLogic _aFL;
+        private Admin _admin;
+        private string _username;
+        private string _password;
+
+        [SetUp]
+        public void Setup()
+        {
+            _lC = new();
+            _aFL = new();
+
+            _username = "admin";
+            _password = "admin123";
+        }
+
+        [Test]
+        public void IntegrationTest_AdminLoginAndGetReport()
+        {
+            //Check if _admin object is null before passing to LoginReturnAdmin()
+            Assert.IsNull(_admin);
+
+            //Check if _admin object is not null before passing to LoginReturnAdmin()
+            _admin = _lC.LoginReturnAdmin(_username, _password);
+            Assert.IsNotNull(_admin);
+
+            //_aFL.SoldTicketsBetweenSpecificDates(_admin,)
+        }
+    }
+}
---
+++ ParkeringshusetTests/IntegrationTests/CheckOutAndPayTicketIntegrationTests.cs
@@ -0,0 +1,59 @@
+﻿namespace ParkeringshusetTests.IntegrationTests
+{
+    using NUnit.Framework;
+    using Parkeringshuset.BusinessLogic;
+    using Parkeringshuset.Models;
+
+    [Category("IntegrationTests")]
+    [TestFixture]
+    public class CheckOutAndPayTicketIntegrationTests
+    {
+        private PTicket _ticket;
+        private CreditCard _creditCard;
+        private ParkingTicketController _tC;
+        private PaymentLogic _pL;
+        private string _regNr;
+        private string _pType;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tC = new();
+            _pL = new();
+            _regNr = "qwe123";
+            _pType = "Electric";
+            _creditCard = new();
+            _creditCard.Number = "1234123412341234";
+            _creditCard.CSV = "123";
+            _tC.CreateTicket(_regNr, _pType);
+            _ticket = _tC.GetActiveTicket(_regNr);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _regNr = "";
+            _pType = "";
+            _tC = null;
+            _ticket = null;
+        }
+
+        [Test]
+        public void IntegrationTest_AssertCheckout_AssertPayment()
+        {
+            //Check if ticket is active before passing _ticket to Checkout()
+            Assert.IsTrue(_ticket.isActice);
+
+            //Check if Checkout is successful
+            var checkOutResult = _tC.CheckOut(_ticket);
+            Assert.IsTrue(checkOutResult);
+
+            //Check if ticket.IsActive=false after _ticket is passed to CheckOut()
+            Assert.IsFalse(_ticket.isActice);
+
+            //Check if ticket is paid after _ticket is passed to Payment()
+            _ticket = _pL.Payment(_creditCard, _ticket);
+            Assert.IsTrue(_ticket.IsPaid);
+        }
+    }
+}
